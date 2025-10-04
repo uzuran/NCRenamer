3534dfdff961b79a2b57ebb9e03b83498d3de4de
Uzuran <archloverlinux@gmail.com>
2025-10-04 12:28:34 +0200

    feat: docstring to password model

--- app/models/password_model.py
+++ app/models/password_model.py
@@ -1,6 +1,10 @@
+"""A simple password model."""
+
 class PasswordModel:
+    """A simple password model for demonstration purposes."""
     def __init__(self, correct_password: str):
         self.correct_password = correct_password
 
     def verify_password(self, password: str) -> bool:
+        """Verify if the provided password matches the correct password."""
         return password == self.correct_password
